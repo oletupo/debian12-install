eca4bc1eb38f8715e6a4c67ee409cb37b49ce640
oletupo <46746675+oletupo@users.noreply.github.com>
2023-08-10 11:58:31 +0200

    Update install-qtile.sh

--- install-qtile.sh
+++ install-qtile.sh
@@ -4,6 +4,8 @@
 sudo apt install -y git python3-pip python3-xcffib python3-cairocffi pipx
 # qtile download and installation
 pipx install "git+https://github.com/qtile/qtile.git"
+# add to qtile enviorment the psutil package for widget use
+pipx inject qtile psutil
 pipx ensurepath
 # qtile registry for lightdm
 cat > ./temp << "EOF"
@@ -18,7 +20,7 @@ u=$USER
 sudo echo "Exec=/home/$u/.local/bin/qtile start" | sudo tee -a /usr/share/xsessions/qtile.desktop
 
 # Packages needed qtile after installation
-sudo apt install -y picom rofi qalculate-gtk vim geany geany-plugin-treebrowser mpv lxappearance arc-theme udiskie lxpolkit ntfs-3g
+sudo apt install -y picom alacritty rofi qalculate-gtk vim geany geany-plugin-treebrowser mpv lxappearance arc-theme udiskie lxpolkit ntfs-3g
 
 # copy configuration files
 cp -Rf .config ~/
